8c068be02664c2a74f71cc56c496ac95891a7c20
peter9217 <peter9217@naver.com>
2024-02-06 05:24:33 +0900

    0206

---
+++ src/main/java/nasaro/NasaroApplication.java
@@ -0,0 +1,13 @@
+package nasaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nasaro/NasaroApplicationTests.java
@@ -0,0 +1,13 @@
+package nasaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
